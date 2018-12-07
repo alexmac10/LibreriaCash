9a7bc29668a7f35b9f55677956a73ddb8fe8188a
Alex <fpalma@siccob.com.mx>
2018-12-07 16:45:14 -0600

    Se empiezan desarrollo de test de prueba

---
+++ LibreriaKioscoCash/Exceptions/CashException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaKioscoCash.Exceptions
+{
+    public class CashException : Exception
+    {
+        private int[] countCash;
+
+        public CashException(int[] count)
+        {
+            countCash = count;
+        }
+
+        public int[] getInformationCashNotDeliveredException()
+        {
+            return countCash;
+        }
+    }
+}
