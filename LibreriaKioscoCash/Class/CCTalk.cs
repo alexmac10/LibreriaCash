35be4569ae0e5cc5b8b0e007af0e7352af06227c
Jesus <josornio@siccob.com.mx>
2018-12-10 13:26:09 -0600

    Incorporando Dispositivo ComboT

--- LibreriaKioscoCash/Class/CCTalk.cs
+++ LibreriaKioscoCash/Class/CCTalk.cs
@@ -75,7 +75,7 @@ namespace LibreriaKioscoCash.Class
                 TX += parameters[i] + " ";
             }
             //Console.WriteLine(TX);
-            Console.WriteLine("TX: " + ByteArrayToString(parameters));
+            //Console.WriteLine("TX: " + ByteArrayToString(parameters));
 
             Thread.Sleep(50);
         }
@@ -93,7 +93,7 @@ namespace LibreriaKioscoCash.Class
                 RX += result[i] + " ";
             }
             //Console.WriteLine(RX);
-            Console.WriteLine("RX: " + ByteArrayToString(resultmessage));
+            //Console.WriteLine("RX: " + ByteArrayToString(resultmessage));
             Thread.Sleep(150);
 
         }
@@ -182,28 +182,28 @@ namespace LibreriaKioscoCash.Class
         //        this.getMessage(parameter);
         //    }
 
-        //    //Regresa el Checksum
-        //    private List<byte> setChecksum(List<byte> parameter, List<byte> data)
+        //Regresa el Checksum
+        //private List<byte> setChecksum(List<byte> parameter, List<byte> data)
+        //{
+        //    byte sum = 0;
+        //    if (data.Count > 0)
         //    {
-        //        byte sum = 0;
-        //        if (data.Count > 0)
-        //        {
-        //            parameter[1] = (byte)data.Count;
-        //        }
-
-        //        parameter.AddRange(data);
-
-        //        foreach (byte i in parameter)
-        //        {
-        //            sum += i;
-        //        }
-
-        //        sum = (byte)(256 - (sum % 256));
-
-        //        parameter.Add(sum);
-
-        //        return parameter;
+        //        parameter[1] = (byte)data.Count;
         //    }
+
+        //    parameter.AddRange(data);
+
+        //    foreach (byte i in parameter)
+        //    {
+        //        sum += i;
+        //    }
+
+        //    sum = (byte)(256 - (sum % 256));
+
+        //    parameter.Add(sum);
+
+        //    return parameter;
+        //}
 
         //    //Envia el mensaje al dispositivo
         //    private void sendMessage(List<byte> parameters)
@@ -240,142 +240,142 @@ namespace LibreriaKioscoCash.Class
         //        //Console.WriteLine(RX);            
         //    }
 
-        //    /*
-        //     * Encargado de definir por default la denominacion para todos los 
-        //     * contenedores de monedas
-        //     */
-        //    protected void setDefaultConfigConteinersCoins()
+        /*
+         * Encargado de definir por default la denominacion para todos los 
+         * contenedores de monedas
+         */
+        //protected void setDefaultConfigConteinersCoins()
+        //{
+        //    this.setValueConteinerCoins(new byte[] { 4, 2 });//monedas de 1
+        //    this.setValueConteinerCoins(new byte[] { 5, 0 });//monedas de 2
+        //    this.setValueConteinerCoins(new byte[] { 6, 3 });//monedas de 5
+        //    this.setValueConteinerCoins(new byte[] { 7, 5 });//monedas de 10
+        //}
+
+        ///*
+        //* Encardado de modificar la denominacion que tendra el contenedor de monedas
+        //*/
+        //public void setValueConteinerCoins(byte[] data)
+        //{
+        //    this.setMessage({ 26, 0, 1, 210 }, new
+        //    List<byte>(data));
+        //}
+
+        ////Estableciendo la configuracion inicial para el hooper
+        //public virtual void setConfig()
+        //{
+        //    if (this.device == "COMBOT")
         //    {
-        //        this.setValueConteinerCoins(new byte[] { 4, 2 });//monedas de 1
-        //        this.setValueConteinerCoins(new byte[] { 5, 0 });//monedas de 2
-        //        this.setValueConteinerCoins(new byte[] { 6, 3 });//monedas de 5
-        //        this.setValueConteinerCoins(new byte[] { 7, 5 });//monedas de 10
+        //        this.resetAccepter();
+        //        this.setDefaultConfigConteinersCoins();
+        //        this.setConfigDefault();
+        //    }
+        //    else
+        //    {
+        //        this.resetAccepter();
+        //        this.setConfigDefault();
+        //    }
+        //}
+
+        ///*
+        //* Encargado de recetear el Acceptador para limpiar los datos 
+        //* de la ultima transacción
+        //*/
+        //private void resetAccepter()
+        //{
+        //    if (this.device == "COMBOT")
+        //    {
+        //        this.setMessage(new List<byte>() { 26, 0, 1, 1 }, new List<byte>());
+        //    }
+        //    else
+        //    {
+        //        this.setMessage(new List<byte>() { 2, 0, 1, 1 }, new List<byte>());
+        //    }
+        //}
+
+        ///*
+        // * Encargado de configurar por default las monedas que se van aceptar con las denominaciones:
+        // * $1,$2,$5,$10 
+        // */
+        //private void setConfigDefault()
+        //{
+        //    //Binario para aceptar las monedas
+        //    //[00000000] = 0 no acepta monedas
+        //    //[00000001] = 1 acepta $0.10 (coin 1)
+        //    //[00000010] = 2 acepta $0.20 (coin 2)
+        //    //[00000100] = 6 acepta $0.50 (coin 3)
+        //    //[00001000] = 8 acepta $1 (coin 4)
+        //    //[00010000] = 16 acepta $2 (coin 5)
+        //    //[00100000] = 32 acepta $5 (coin 6)
+        //    //[01000000] = 64 acepta $10 (coin 7)
+        //    //[10000000] = 128 acepta $20 (coin 8)
+
+        //    //Para agregar solo las $10,$5,$2 $1 mandar 120 
+        //    //[01111000] = 120 
+        //    // El data queda [120,255] el 255 siempres se va a poner
+        //    if (this.device == "COMBOT")
+        //    {
+        //        this.setMessage(new List<byte>() { 26, 0, 1, 231 }, new
+        //    List<byte>() { 120, 255 });
+        //    }
+        //    else
+        //    {
+        //        this.setMessage(new List<byte>() { 2, 0, 1, 231 }, new
+        //   List<byte>() { 124, 255 });
         //    }
 
-        //    /*
-        //    * Encardado de modificar la denominacion que tendra el contenedor de monedas
-        //    */
-        //    public void setValueConteinerCoins(byte[] data)
+        //}
+
+        ///*
+        // * Obtiene los id que tienen los dispositivos 
+        // */
+        //public void getIdDevice()
+        //{
+        //    this.setMessage(new List<byte>() { 0, 0, 1, 253 }, new
+        //    List<byte>());
+        //}
+
+        //public void emptyMoneyBox()
+        //{
+        //    //si data = 1 se vacia por atras
+        //    //si data = 2 se vacia por enfrente
+        //    this.setMessage(new List<byte>() { 12, 0, 1, 70 }, new
+        //        List<byte>() { 1 });
+        //}
+        //private void emptyContainerCoin(byte device, byte count = 255)
+        //{
+        //    this.enableContainerCoin(device);
+        //    Thread.Sleep(500);
+        //    byte[] serie = this.getNumberSerie(device);
+        //    serie[3] = count; //Define la cantidad que debe vaciar
+        //    this.setMessage(new List<byte>() { device, 0, 1, 167 }, new
+        //    List<byte>(serie));
+        //    Thread.Sleep(500);
+        //}
+
+        ///*
+        // * Encargado de habilitar el contenedor de monedas.
+        // */
+        //private void enableContainerCoin(byte device)
+        //{
+        //    this.setMessage(new List<byte>() { device, 0, 1, 164 }, new
+        //    List<byte>() { 165 });
+        //}
+
+        ///*
+        //* Encargado de obtener los numero de serie del dispositvo
+        //*/
+        //private byte[] getNumberSerie(byte device)
+        //{
+        //    byte[] serie = new byte[4];
+        //    this.setMessage(new List<byte>() { device, 0, 1, 242 }, new
+        //    List<byte>());
+
+        //    for (int i = 4, j = 0; i < this.resultMessage.Length - 1; i++, j++)
         //    {
-        //        this.setMessage(new List<byte>() { 26, 0, 1, 210 }, new
-        //        List<byte>(data));
+        //        serie[j] = this.resultMessage[i];
         //    }
-
-        //    //Estableciendo la configuracion inicial para el hooper
-        //    public virtual void setConfig()
-        //    {
-        //        if (this.device == "COMBOT")
-        //        {
-        //            this.resetAccepter();
-        //            this.setDefaultConfigConteinersCoins();
-        //            this.setConfigDefault();
-        //        }
-        //        else
-        //        {
-        //            this.resetAccepter();
-        //            this.setConfigDefault();
-        //        }
-        //    }
-
-        //    /*
-        //    * Encargado de recetear el Acceptador para limpiar los datos 
-        //    * de la ultima transacción
-        //    */
-        //    private void resetAccepter()
-        //    {
-        //        if (this.device == "COMBOT")
-        //        {
-        //            this.setMessage(new List<byte>() { 26, 0, 1, 1 }, new List<byte>());
-        //        }
-        //        else
-        //        {
-        //            this.setMessage(new List<byte>() { 2, 0, 1, 1 }, new List<byte>());
-        //        }
-        //    }
-
-        //    /*
-        //     * Encargado de configurar por default las monedas que se van aceptar con las denominaciones:
-        //     * $1,$2,$5,$10 
-        //     */
-        //    private void setConfigDefault()
-        //    {
-        //        //Binario para aceptar las monedas
-        //        //[00000000] = 0 no acepta monedas
-        //        //[00000001] = 1 acepta $0.10 (coin 1)
-        //        //[00000010] = 2 acepta $0.20 (coin 2)
-        //        //[00000100] = 6 acepta $0.50 (coin 3)
-        //        //[00001000] = 8 acepta $1 (coin 4)
-        //        //[00010000] = 16 acepta $2 (coin 5)
-        //        //[00100000] = 32 acepta $5 (coin 6)
-        //        //[01000000] = 64 acepta $10 (coin 7)
-        //        //[10000000] = 128 acepta $20 (coin 8)
-
-        //        //Para agregar solo las $10,$5,$2 $1 mandar 120 
-        //        //[01111000] = 120 
-        //        // El data queda [120,255] el 255 siempres se va a poner
-        //        if (this.device == "COMBOT")
-        //        {
-        //            this.setMessage(new List<byte>() { 26, 0, 1, 231 }, new
-        //        List<byte>() { 120, 255 });
-        //        }
-        //        else
-        //        {
-        //            this.setMessage(new List<byte>() { 2, 0, 1, 231 }, new
-        //       List<byte>() { 124, 255 });
-        //        }
-
-        //    }
-
-        //    /*
-        //     * Obtiene los id que tienen los dispositivos 
-        //     */
-        //    public void getIdDevice()
-        //    {
-        //        this.setMessage(new List<byte>() { 0, 0, 1, 253 }, new
-        //        List<byte>());
-        //    }
-
-        //    public void emptyMoneyBox()
-        //    {
-        //        //si data = 1 se vacia por atras
-        //        //si data = 2 se vacia por enfrente
-        //        this.setMessage(new List<byte>() { 12, 0, 1, 70 }, new
-        //            List<byte>() { 1 });
-        //    }
-        //    private void emptyContainerCoin(byte device, byte count = 255)
-        //    {
-        //        this.enableContainerCoin(device);
-        //        Thread.Sleep(500);
-        //        byte[] serie = this.getNumberSerie(device);
-        //        serie[3] = count; //Define la cantidad que debe vaciar
-        //        this.setMessage(new List<byte>() { device, 0, 1, 167 }, new
-        //        List<byte>(serie));
-        //        Thread.Sleep(500);
-        //    }
-
-        //    /*
-        //     * Encargado de habilitar el contenedor de monedas.
-        //     */
-        //    private void enableContainerCoin(byte device)
-        //    {
-        //        this.setMessage(new List<byte>() { device, 0, 1, 164 }, new
-        //        List<byte>() { 165 });
-        //    }
-
-        //    /*
-        //    * Encargado de obtener los numero de serie del dispositvo
-        //    */
-        //    private byte[] getNumberSerie(byte device)
-        //    {
-        //        byte[] serie = new byte[4];
-        //        this.setMessage(new List<byte>() { device, 0, 1, 242 }, new
-        //        List<byte>());
-
-        //        for (int i = 4, j = 0; i < this.resultMessage.Length - 1; i++, j++)
-        //        {
-        //            serie[j] = this.resultMessage[i];
-        //        }
-        //        return serie;
-        //    }
+        //    return serie;
+        //}
     }
 }
