3bcc0bd4d41223163c2db30b2e251627eaeb14ed
Alex <fpalma@siccob.com.mx>
2018-12-10 10:12:31 -0600

    Se define menu para el sistema de pruebas

--- Test/Program.cs
+++ Test/Program.cs
@@ -25,66 +25,67 @@ namespace Test
 
         static void Main(string[] args)
         {
-            IDispenser billDispenser = factory.GetBillDispenser();
-            IAcceptor billAcceptor = factory.GetBillAcceptor();
-            IAcceptor coinAcceptor = factory.GetCoinAcceptor();
-            IDispenser coinDispenser = factory.GetCoinDispenser();
+            int test = 0;
+            bool continuar = true;
             
-
-            try
+           
+            while (continuar)
             {
-                //Habilitando dispositivo
-                coinAcceptor.open();
-                coinDispenser.open();
-                billAcceptor.open();
-                billDispenser.open();
-
-                int count = 0;
-                while (count < 10)
+                Console.Clear();
+                Console.WriteLine("***************** MENU PRINCIPAL ******************");
+                Console.WriteLine("Indique el tipo de prueba que desea realizar: ");
+                Console.WriteLine("     1) Prueba con dispositivo Bill Dispenser ");
+                Console.WriteLine("     2) Prueba con dispositivo Bill Acceptor ");
+                Console.WriteLine("     3) Prueba con dispositivo Coin Dispenser ");
+                Console.WriteLine("     4) Prueba con dispositivo Coin Acceptor ");
+                Console.WriteLine("     5) Prueba con todos los dispositivos ");
+                Console.WriteLine("     6) Salir ");
+                Console.WriteLine();
+                Console.Write("     Indique la opción: ");
+                try
                 {
-                    if (!billAcceptor.isConnection())
-                    {
-                        continue;
-                    }
-                    billAcceptor.enable();
-                    Console.WriteLine();
-                    Console.WriteLine("*****************RETIRO DE EFECTIVO******************");
-                    Console.Write("Indique la cantidad de billetes de a $20.00 a retirar: ");
-                    string cantidad20 = Console.ReadLine();
-                    Console.Write("Indique la cantidad de billetes de a $50.00 a retirar: ");
-                    string cantidad50 = Console.ReadLine();
-                    Console.Write("Indique la cantidad de billetes de a $100.00 a retirar: ");
-                    string cantidad100 = Console.ReadLine();
-                    int[] BillCount = { Int32.Parse(cantidad20), Int32.Parse(cantidad50), Int32.Parse(cantidad100) };
-                    foreach (var b in billDispenser.returnCash(0, 0, BillCount))
-                    {
-                        Console.WriteLine(b);
-                    }
-                    Console.WriteLine("*****************************************************");
-                    
-                   
-                    if (count == 9)
-                    {
-                        billAcceptor.disable();
-                        billDispenser.close();
-                    }
-                    //Console.WriteLine(count);
-                    Thread.Sleep(2000);
-                    count++;
+                    test = Int32.Parse(Console.ReadLine());
                 }
-                
+                catch (Exception ex)
+                {
+                    test = 0;
+                }
 
+                switch (test)
+                {
+                    case 1:
 
+                        break;
+                    case 2:
 
+                        break;
+                    case 3:
 
+                        break;
+                    case 4:
 
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Debe seleccionar una opcion del menu ");
+                        break;
+                }
+
+                Console.WriteLine("¿Quieres ingresar con otra opción del menu (y/n)?");
+                string respuesta = Console.ReadLine();
+
+                if(respuesta == "n" || respuesta == "N")
+                {
+                    continuar = false;
+                }
+                                        
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-
+                                  
         }
 
         /// <summary>
@@ -124,11 +125,11 @@ namespace Test
                 /// el tercer parametro es un arreglo tipo int y debera definir en el orden
                 /// para los billetes que son : [20,50,100]
                 ///</remarks>                
-                billDispenser.returnCash(0,0,billCount);
+                billDispenser.returnCash(0, 0, billCount);
 
                 //Cierra conexion con dispositivo 
                 billDispenser.close();
-            }           
+            }
             catch (CashException ex)
             {
                 ///<remarks>
@@ -145,6 +146,7 @@ namespace Test
                 ///<remarks>
                 ///Se recibe la excepción con el error de conexión
                 ///</remarks>
+                Console.WriteLine(ex.Message);
             }
 
         }
@@ -197,9 +199,8 @@ namespace Test
         /// Pruebas con dispositivo para entregar monedas
         /// </summary>
         static void testCoinDispenser()
-        {
-
-           
+        {                                   
+            IDispenser coinDispenser = factory.GetCoinDispenser();
 
         }
 
@@ -208,7 +209,7 @@ namespace Test
         /// </summary>
         static void testCoinAcceptor()
         {
-           
+            IAcceptor coinAcceptor = factory.GetCoinAcceptor();
 
 
         }
